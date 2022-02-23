adb6d287810b732a903959045e66a215e700a1f0
Evan <83352674+BiigFish@users.noreply.github.com>
2022-02-23 12:04:26 -0500

    Initial commit

---
+++ src/styles/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['public/**/*.html'],
+  safelist: [],
+  theme: {
+    extend: {
+      colors: {
+        color1: 'red',
+        color2: 'green'
+      },
+    },
+  },
+  plugins: [],
+}
