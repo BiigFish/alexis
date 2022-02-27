0239fa095d45562ae9a4895c334968bf4342a87c
Evan <evchan29@gmail.com>
2022-02-27 10:42:55 -0500

    Draft 1
    RIP sorted colors with tags

--- src/styles/tailwind.config.js
+++ src/styles/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   safelist: [],
   theme: {
     extend: {
+      fontFamily: {
+        "oooh-baby":['"Oooh Baby"', 'cursive']
+      },
       colors: {
         textColor: 'rgb(100 116 139)',
         queenBlue: '#577590',
@@ -10,6 +13,7 @@ module.exports = {
         blond: '#FCF6B1',
         uranianBlue: '#A4DEF9',
         pistachio: '#8DC97E',
+        beige: '#FFFFE8',
       },
       boxShadow: {
         'old': '10px 10px LightGray'
